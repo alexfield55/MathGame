28381fd792b01c03fb71ef9db5dfec0cafeefde4
Alex Field <38266956+alexfield55@users.noreply.github.com>
2021-11-24 13:41:17 -0700

    fixed db connection

--- MathGame/Pages/Index.cshtml.cs
+++ MathGame/Pages/Index.cshtml.cs
@@ -1,28 +1,20 @@
 ﻿using ApplicationCore.Interfaces;
 using ApplicationCore.Models;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MathGame.Pages
 {
     public class IndexModel : PageModel
     {
-        //private readonly IUnitOfWork _unitOfWork;
-        private readonly ILogger<IndexModel> _logger;
+        public IUnitOfWork _unitOfWork { get; set; }
 
         [BindProperty]
         public Player PlayerObj { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IUnitOfWork unitOfWork)
         {
-           // _unitOfWork = unitOfWork;
-            _logger = logger;
+           _unitOfWork = unitOfWork;
         }
 
         public void OnGet()
@@ -30,22 +22,25 @@ namespace MathGame.Pages
 
         }
 
-        public IActionResult OnPost()
+        public void OnPost()
         {
             if (!ModelState.IsValid)
             {
-                return Page();
+                Response.Redirect("/Index");
             }
 
             //add new player in
-            //_unitOfWork.Players.Add(new Player
-            //{
-            //    Name = PlayerObj.Name,
-            //    Age = PlayerObj.Age
-            //});
+            _unitOfWork.Players.Add(new Player
+            {
+                Name = PlayerObj.Name,
+                Age = PlayerObj.Age
+            });
+
+            Player p = new Player();
+            p = _unitOfWork.Players.Get(p => p.Name == PlayerObj.Name);
 
             //go to game page
-            return RedirectToPage("/Game/GameIndex");
+            Response.Redirect("/Game/GameIndex?id="+p.id);
         }
     }
 }
--- MathGame/Startup.cs
+++ MathGame/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Infrastructure.Data;
+using ApplicationCore.Interfaces;
 
 namespace MathGame
 {
@@ -25,7 +26,7 @@ namespace MathGame
             services.AddDbContext<MathGameContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("MathGameContext")));
 
-
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
